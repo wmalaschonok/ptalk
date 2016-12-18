791cd34b49c87d11d4b67d996d480b0f45f30760
dromedarCase <davidmalaschonok@gmail.com>
2016-12-18 17:17:16 +0100

    Refactoring in api.js.

--- server/api.js
+++ server/api.js
@@ -6,14 +6,25 @@ const port = 4242;
 const pwd = process.argv[2];
 
 const server = http.createServer((req, res) => {
-  //res.statusCode = 200;
-  //res.setHeader('Content-Type', 'application/json');
-  var query = req.url;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+
+  //parsing happens here
+  query = req.url;
   query = query.replace(/\//g,' ');
   query = query.replace(/_a/g,'{');
   query = query.replace(/_b/g,'}');
 
-  let aw = request({
+  cypher(query, res);
+});
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
+
+function cypher(query, res) {
+    console.log("Cypher: " + query);
+    request({
     uri: "http://neo4j:"+pwd+"@localhost:7474/db/data/transaction/commit",
     headers: [
     {
@@ -32,14 +43,6 @@ const server = http.createServer((req, res) => {
         } ]
     }
     }, function(error, request, body) {
-        result = body;
-  });
-
-  //res.end(JSON.stringify(result));
-  aw.pipe(res);
-  console.log('Someone queried \''+query+'\'.');
-});
-
-server.listen(port, () => {
-  console.log(`Query me at port ${port}.`);
-});
+        res.end(JSON.stringify(body));
+    });
+}
