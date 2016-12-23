5b3d1f06d96e120f0a553442ec789a10b59850bf
Wladimir Malaschonok <w.malashonok@gmail.com>
2016-12-23 17:25:25 +0100

    CHANGED: Build and run scripts. Dirs will be created. Cmd call is used to avoid "dropped cmd lines"

--- build.bat
+++ build.bat
@@ -1,15 +1,20 @@
-@echo off
+rem @echo off
 rmdir bld /S /Q
 mkdir bld
 
-xcopy server\winPatch\neo4j-community-3.1.1-SNAPSHOT bld\neo4j-community-3.1.1-SNAPSHOT /E /Y
-xcopy server\dump bld\dump /E /Y
+mkdir .\bld\neo4j-community-3.1.1-SNAPSHOT
+mkdir .\bld\dump
+
+xcopy server\winPatch\neo4j-community-3.1.1-SNAPSHOT .\bld\neo4j-community-3.1.1-SNAPSHOT /E /Y
+xcopy server\dump .\bld\dump /E /Y
 
 cd bld
 ::	rmdir neo4j-community-3.1.1-SNAPSHOT\data\databases\graph.db /S /Q
 ::	move dump neo4j-community-3.1.1-SNAPSHOT\data\databases\graph.db /Y
-	neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat start
-		echo "Please open http://localhost:7474 and change the default password. Then press a key."
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat install-service
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat start
+		echo "IMPORTANT! Please open in browser http://localhost:7474 and CHANGE THE DEFAULT PASSWORD. Then press a key."
 		pause
-	neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat stop
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat stop
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat uninstall-service
 cd ..
--- run.bat
+++ run.bat
@@ -1,6 +1,10 @@
 @echo off
-bld\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat stop
-bld\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat start
+cd bld
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat stop
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat uninstall-service
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat install-service
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j.bat start
 echo "Press a key to stop."
 pause
-bld\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j stop
+call .\neo4j-community-3.1.1-SNAPSHOT\bin\neo4j stop
+cd ..
